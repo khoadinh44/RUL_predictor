643b6e7c16e604ecf454cd396f267e1e7c321617
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-05-19 21:08:31 +0700

    Update dnn.py

--- model/dnn.py
+++ model/dnn.py
@@ -9,15 +9,19 @@ from keras.models import Model
 
 def dnn_model(opt):
   input_ = keras.layers.Input(shape=[opt.input_shape, ])
-  hidden1 = keras.layers.Dense(300, activation=tf.keras.layers.ReLU(), 
+  x = keras.layers.Dense(1024, activation=tf.keras.layers.ReLU(), 
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(input_)
-  hidden2 = keras.layers.Dense(100,activation=tf.keras.layers.ReLU(), 
+  x = keras.layers.Dense(512,activation=tf.keras.layers.ReLU(), 
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
-                                     activity_regularizer=regularizers.l2(1e-5))(hidden1)
-  concat = keras.layers.concatenate([input_, hidden2])
-  output = keras.layers.Dense(opt.num_classes, activation=tf.keras.layers.Softmax())(concat)
+                                     activity_regularizer=regularizers.l2(1e-5))(x)
+  x = keras.layers.Dense(256,activation=tf.keras.layers.ReLU(), 
+                                     kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+                                     bias_regularizer=regularizers.l2(1e-4),
+                                     activity_regularizer=regularizers.l2(1e-5))(x)
+  # concat = keras.layers.concatenate([input_, hidden2])
+  output = keras.layers.Dense(opt.num_classes, activation=tf.keras.layers.Softmax())(x)
   model = keras.models.Model(inputs=[input_], outputs=[output])
   return model
