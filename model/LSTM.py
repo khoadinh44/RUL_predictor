885468b2c3a4117cddc4c6a2831974bd00167889
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-05-25 14:06:59 +0700

    Update LSTM.py

--- model/LSTM.py
+++ model/LSTM.py
@@ -1,12 +1,17 @@
 from tensorflow.keras.layers import Conv1D, Activation, Dense, concatenate, BatchNormalization, GlobalAveragePooling1D, Input, MaxPooling1D, Lambda, GlobalAveragePooling2D, ReLU, MaxPooling2D, Flatten, Dropout, LSTM
 from keras.models import Model
+import tensorflow as tf
 
 def lstm_model(opt):
   inputs = Input(shape=[opt.input_shape, 2])
   x = LSTM(units=100, return_sequences=True)(inputs)
+  x = tf.keras.activations.tanh(x)
   x = Dropout(0.2)(x)
-  x = LSTM(units=50, return_sequences=False)(x)
+  x = LSTM(units=50, return_sequences=True)(x)
+  x = tf.keras.activations.tanh(x)
   x = Dropout(0.2)(x)
+  x = GlobalAveragePooling1D(data_format='channels_first', keepdims=False)(x)
+  x = Dense(1024, activation='tanh')(x)
   x = Dense(units=opt.num_classes, activation='sigmoid')(x)
-  m = Model(inputs, x, name='resnet34')
+  m = Model(inputs, x)
   return m
