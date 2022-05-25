988ecd7a61d6a2f1b1fb844b4726454502ee8a68
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-05-25 19:07:42 +0700

    Update dnn.py

--- model/dnn.py
+++ model/dnn.py
@@ -9,10 +9,20 @@ from keras.models import Model
 
 def dnn_model(opt):
   input_ = keras.layers.Input(shape=[opt.input_shape*2, ])
-  fc1 = keras.layers.Dense(2048, activation=tf.keras.layers.ReLU(), 
+  fc1 = keras.layers.Dense(5092, activation=tf.keras.layers.ReLU(), 
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(input_)
+  fc1 = Dropout(0.2)(fc1)
+  fc1 = keras.layers.Dense(2048,activation=tf.keras.layers.ReLU(), 
+                                     kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+                                     bias_regularizer=regularizers.l2(1e-4),
+                                     activity_regularizer=regularizers.l2(1e-5))(fc1)
+  fc1 = Dropout(0.2)(fc1)
+  fc1 = keras.layers.Dense(2048,activation=tf.keras.layers.ReLU(), 
+                                     kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+                                     bias_regularizer=regularizers.l2(1e-4),
+                                     activity_regularizer=regularizers.l2(1e-5))(fc1)
   fc1 = Dropout(0.2)(fc1)
   fc1 = keras.layers.Dense(1024,activation=tf.keras.layers.ReLU(), 
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
@@ -24,16 +34,27 @@ def dnn_model(opt):
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(fc1)
   fc1 = Dropout(0.2)(fc1)
-  fc1 = keras.layers.Dense(256,activation=tf.keras.layers.ReLU(), 
+  fc1 = keras.layers.Dense(512,activation=tf.keras.layers.ReLU(), 
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(fc1)
+  fc1 = BatchNormalization()(fc1)
   fc1 = Dropout(0.2)(fc1)
 
-  fc2 = keras.layers.Dense(2048, activation=tf.keras.layers.ReLU(), 
+  fc2 = keras.layers.Dense(5092, activation=tf.keras.layers.ReLU(), 
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(input_)
+  fc2 = Dropout(0.2)(fc2)
+  fc2 = keras.layers.Dense(2048,activation=tf.keras.layers.ReLU(), 
+                                     kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+                                     bias_regularizer=regularizers.l2(1e-4),
+                                     activity_regularizer=regularizers.l2(1e-5))(fc2)
+  fc2 = Dropout(0.2)(fc2)
+  fc2 = keras.layers.Dense(2048,activation=tf.keras.layers.ReLU(), 
+                                     kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+                                     bias_regularizer=regularizers.l2(1e-4),
+                                     activity_regularizer=regularizers.l2(1e-5))(fc2)
   fc2 = Dropout(0.2)(fc2)
   fc2 = keras.layers.Dense(1024,activation=tf.keras.layers.ReLU(), 
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
@@ -44,11 +65,7 @@ def dnn_model(opt):
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(fc2)
-  fc2 = Dropout(0.2)(fc2)
-  fc2 = keras.layers.Dense(256,activation=tf.keras.layers.ReLU(), 
-                                     kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
-                                     bias_regularizer=regularizers.l2(1e-4),
-                                     activity_regularizer=regularizers.l2(1e-5))(fc2)
+  fc2 = BatchNormalization()(fc2)
   fc2 = Dropout(0.2)(fc2)
   x = concatenate([fc1, fc2])
   if opt.rul_train:
