d268fd75bea1183444da9ef54619ea02d4963675
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-05-25 17:27:57 +0700

    Update train.py

--- train.py
+++ train.py
@@ -58,7 +58,7 @@ def main(opt, train_data, train_label, test_data, test_label):
   if opt.condition_train:
     network.compile(optimizer=AngularGrad(1e-4), loss='categorical_crossentropy', metrics=['acc', f1_m, precision_m, recall_m]) # loss='mse'
   if opt.rul_train:
-    network.compile(optimizer=AngularGrad(), loss='mean_squared_error', metrics=['mae', r2_keras, tf.keras.metrics.mean_squared_error]) # loss='mse' tf.keras.optimizers.RMSprop 'binary_crossentropy'
+    network.compile(optimizer=AngularGrad(1e-4), loss='mean_squared_error', metrics=['mae', r2_keras, tf.keras.metrics.mean_squared_error]) # loss='mse' tf.keras.optimizers.RMSprop 'binary_crossentropy'
   network.summary()
   history = network.fit(train_data, train_label,
                       epochs     = opt.epochs,
