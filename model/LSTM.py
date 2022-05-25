3bb77df9ce632f11665d1fe104ba85607cb5645a
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-05-25 11:47:51 +0700

    Update LSTM.py

--- model/LSTM.py
+++ model/LSTM.py
@@ -1,7 +1,7 @@
 from tensorflow.keras.layers import Conv1D, Activation, Dense, concatenate, BatchNormalization, GlobalAveragePooling1D, Input, MaxPooling1D, Lambda, GlobalAveragePooling2D, ReLU, MaxPooling2D, Flatten, Dropout, LSTM
 
-def lstm_model(opt, input_shape):
-  inputs = Input(shape=input_shape)
+def lstm_model(opt):
+  inputs = Input(shape=[opt.input_shape, 2])
   x = LSTM(units=100, return_sequences=True)(x)
   x = Dropout(0.2)(x)
   x = LSTM(units=50, return_sequences=False)(x)
