5afde46c1da1d6562b08ea587199040eac0881b3
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-05-26 16:19:35 +0700

    Update save_data.py

--- utils/save_data.py
+++ utils/save_data.py
@@ -4,13 +4,14 @@ import pandas as pd
 import pickle as pkl
 from utils.tools import read_data_as_df, process
 
-def start_save_data():
+def start_save_data(opt):
   DATA_POINTS_PER_FILE=2560
   train_names = ['Bearing1_1', 'Bearing1_2', 'Bearing2_1', 'Bearing2_2', 'Bearing3_1', 'Bearing3_2']
   test_names = ['Bearing1_3', 'Bearing1_4', 'Bearing1_5', 'Bearing1_6', 'Bearing1_7', 'Bearing2_3', 'Bearing2_4', 'Bearing2_5', 'Bearing2_6', 'Bearing2_7', 'Bearing3_3']
 
-  # main_dir_colab = '/content/drive/Shareddrives/newpro112233/company/PRONOSTIA/data/'
-  main_dir_colab = '/content/drive/MyDrive/Khoa/data/'
+  main_dir_colab = opt.main_dir_colab
+  # '/content/drive/MyDrive/Khoa/data/'
+  # '/content/drive/Shareddrives/newpro112233/company/PRONOSTIA/data/'
   train_main_dir = main_dir_colab + 'Learning_set/'
   test_main_dir = main_dir_colab + 'Test_set/'
   if os.path.exists(train_main_dir + train_names[0] + '.pkz')==False:
