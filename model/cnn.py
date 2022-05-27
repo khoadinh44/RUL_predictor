6a2394413e7de80b771fed0995f521211c288f2f
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-05-27 10:04:05 +0700

    Update cnn.py

--- model/cnn.py
+++ model/cnn.py
@@ -40,7 +40,7 @@ def identity_block(input_tensor, kernel_size, filters, stage, block):
               bias_regularizer=regularizers.l2(1e-4),
               activity_regularizer=regularizers.l2(1e-5),
               name=conv_name_base + '2a')(input_tensor)
-    x = BatchNormalization(name=bn_name_base + '2a')(x)
+    x = BatchNormalization(training=training, name=bn_name_base + '2a')(x)
     x = Activation('relu')(x)
     x = Dropout(0.2)(x)
 
@@ -52,19 +52,19 @@ def identity_block(input_tensor, kernel_size, filters, stage, block):
               bias_regularizer=regularizers.l2(1e-4),
               activity_regularizer=regularizers.l2(1e-5),
               name=conv_name_base + '2b')(x)
-    x = BatchNormalization(name=bn_name_base + '2b')(x)
+    x = BatchNormalization(training=training, name=bn_name_base + '2b')(x)
 
     if input_tensor.shape[2] != x.shape[2]:
         x = layers.add([x, Lambda(lambda y: K.repeat_elements(y, rep=2, axis=2))(input_tensor)])
     else:
         x = layers.add([x, input_tensor])
 
-    x = BatchNormalization()(x)
+    x = BatchNormalization(training=training)(x)
     x = Activation('relu')(x)
     x = Dropout(0.2)(x)
     return x
   
-def cnn_1d_model(opt):
+def cnn_1d_model(opt, training=None):
     '''
     The model was rebuilt based on the construction of resnet 34 and inherited from this source code:
     https://github.com/philipperemy/very-deep-convnets-raw-waveforms/blob/master/model_resnet.py
@@ -77,28 +77,28 @@ def cnn_1d_model(opt):
                padding='same',
                kernel_initializer='glorot_uniform',
                kernel_regularizer=regularizers.l2(l=0.0001),)(x)
-    x = BatchNormalization()(x)
+    x = BatchNormalization(training=training)(x)
     x = Activation('relu')(x)
     x = MaxPooling1D(pool_size=4, strides=None)(x)
 
 
     for i in range(3):
-        x = identity_block(x, kernel_size=3, filters=48, stage=1, block=i)
+        x = identity_block(x, kernel_size=3, filters=48, stage=1, block=i, training=training)
 
     x = MaxPooling1D(pool_size=4, strides=None)(x)
 
     for i in range(4):
-        x = identity_block(x, kernel_size=3, filters=96, stage=2, block=i)
+        x = identity_block(x, kernel_size=3, filters=96, stage=2, block=i, training=training)
 
     x = MaxPooling1D(pool_size=4, strides=None)(x)
 
     for i in range(23):
-        x = identity_block(x, kernel_size=3, filters=192, stage=3, block=i)
+        x = identity_block(x, kernel_size=3, filters=192, stage=3, block=i, training=training)
 
     x = MaxPooling1D(pool_size=4, strides=None)(x)
 
     for i in range(3):
-        x = identity_block(x, kernel_size=3, filters=384, stage=4, block=i)
+        x = identity_block(x, kernel_size=3, filters=384, stage=4, block=i, training=training)
 
     x = GlobalAveragePooling1D()(x)
     
@@ -126,7 +126,7 @@ def cnn_2d_model(opt, input_shape=[128, 128, 2]):
             Dense(units=256, activation='relu'),
             Dropout(0.5)])
   if opt.rul_train:
-    model.add(Dense(units=opt.num_classes, activation='relu'))
+    model.add(Dense(units=opt.num_classes, activation='sigmoid'))
   if opt.condition_train:
     model.add(Dense(units=opt.num_classes, activation='softmax'))
   return model
