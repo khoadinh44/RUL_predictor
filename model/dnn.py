f4a8f9131d16909d03db87d4054c0a09854c4672
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-05-28 21:39:07 +0700

    Update dnn.py

--- model/dnn.py
+++ model/dnn.py
@@ -8,11 +8,12 @@ from keras import layers, regularizers
 from keras.models import Model
 
 def dnn_model(opt):
-  input_ = keras.layers.Input(shape=[opt.input_shape*2, ])
+  input_1 = keras.layers.Input(shape=[opt.input_shape, ])
+  input_2 = keras.layers.Input(shape=[opt.input_shape, ])
   fc1 = keras.layers.Dense(5092, activation=tf.keras.layers.ReLU(), 
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
-                                     activity_regularizer=regularizers.l2(1e-5))(input_)
+                                     activity_regularizer=regularizers.l2(1e-5))(input_1)
   fc1 = Dropout(0.2)(fc1)
   fc1 = keras.layers.Dense(2048,activation=tf.keras.layers.ReLU(), 
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
@@ -44,7 +45,7 @@ def dnn_model(opt):
   fc2 = keras.layers.Dense(5092, activation=tf.keras.layers.ReLU(), 
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
-                                     activity_regularizer=regularizers.l2(1e-5))(input_)
+                                     activity_regularizer=regularizers.l2(1e-5))(input_2)
   fc2 = Dropout(0.2)(fc2)
   fc2 = keras.layers.Dense(2048,activation=tf.keras.layers.ReLU(), 
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
@@ -72,5 +73,5 @@ def dnn_model(opt):
     output = Dense(opt.num_classes, activation='sigmoid')(x)
   if opt.condition_train:
     output = Dense(opt.num_classes, activation='softmax')(x)
-  model = keras.models.Model(inputs=[input_], outputs=[output])
+  model = keras.models.Model(inputs=[input_1, input_2], outputs=[output])
   return model
