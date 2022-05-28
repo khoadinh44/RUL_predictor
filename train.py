3e46c517ffa50b7a5c2e513105d8100f9a940ef3
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-05-28 21:36:07 +0700

    Update train.py

--- train.py
+++ train.py
@@ -42,8 +42,8 @@ def main(opt, train_data, train_label, test_data, test_label):
 
   if opt.model == 'dnn':
     train_data = [train_data[:, :, 0], train_data[:, :, 1]]
-    test_data = [test_data[:, :, 0], test_data[:, :, 1]]
     val_data, val_label = [test_data[:1000][:, :, 0], test_data[:1000][:, :, 1]]
+    test_data = [test_data[:, :, 0], test_data[:, :, 1]]
     network = dnn_model(opt)
   if opt.model == 'cnn_1d':
     network = cnn_1d_model(opt, training=True)
