968d7de234147231cf2618bde32fd2d8b134edde
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-05-28 20:23:57 +0700

    Update LSTM.py

--- model/LSTM.py
+++ model/LSTM.py
@@ -4,18 +4,12 @@ from keras.models import Model
 
 def lstm_model(opt):
   inputs = Input(shape=[opt.input_shape, 2])
-  x = LSTM(units=256, return_sequences=True)(inputs)
-  x = tf.keras.activations.tanh(x)
-  # x = Dropout(0.2)(x)
-  x = LSTM(units=512, return_sequences=False)(x)
-  x = tf.keras.activations.tanh(x)
-  # x = Dropout(0.2)(x)
-  fc = GlobalAveragePooling1D(data_format='channels_first', keepdims=False)(inputs)
-  fc = Dense(2048, activation='tanh')(fc)
+  x1 = LSTM(units=512, return_sequences=True)(inputs)
+  x1 = LSTM(units=256, return_sequences=False)(x1)
+  fc = Dense(512, activation='relu')(x1)
+  fc = Dense(256, activation='relu')(fc) + x1
   # fc = Dropout(0.2)(fc)
-  fc = Dense(512, activation='tanh')(fc)
-  # fc = Dropout(0.2)(fc)
-  x = concatenate([x, fc])
-  x = Dense(units=opt.num_classes, activation='sigmoid')(x)
-  m = Model(inputs, x)
+#   x = concatenate([x, fc])
+  fc = Dense(units=opt.num_classes, activation='sigmoid')(fc)
+  m = Model(inputs, fc)
   return m
