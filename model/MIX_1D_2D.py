1587e9849ef7620342235030c2fc5975d182243d
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-06-17 09:28:33 +0700

    Update MIX_1D_2D.py

--- model/MIX_1D_2D.py
+++ model/MIX_1D_2D.py
@@ -5,9 +5,6 @@ from keras import layers, regularizers
 import keras.backend as K
 
 def mix_model(opt, model_1D, model_2D, input_1D, input_2D, training):
-#   input_1D = Input((opt.input_shape, 2), name='lstm_input')
-#   input_2D = Input((128, 128, 2), name='CNN_input')
-  
   out_1D = model_1D(opt, training, input_1D)
   out_2D = model_2D(opt)(input_2D, training=training)
   
