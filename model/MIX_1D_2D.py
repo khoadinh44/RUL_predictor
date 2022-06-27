66001ee2a199dae79d6ed1c83611fcf4959dccfb
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-06-27 15:00:22 +0700

    Update MIX_1D_2D.py

--- model/MIX_1D_2D.py
+++ model/MIX_1D_2D.py
@@ -6,22 +6,22 @@ from keras import layers, regularizers
 import keras.backend as K
 
 def TransformerLayer(q, k, v, num_heads=4, training=None):
-    q = tf.keras.layers.Dense(384, activation=tf.keras.layers.ReLU(), 
+    q = tf.keras.layers.Dense(256, activation=tf.keras.layers.ReLU(), 
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(q)
-    k = tf.keras.layers.Dense(384, activation=tf.keras.layers.ReLU(), 
+    k = tf.keras.layers.Dense(256, activation=tf.keras.layers.ReLU(), 
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(k)
-    v = tf.keras.layers.Dense(384, activation=tf.keras.layers.ReLU(), 
+    v = tf.keras.layers.Dense(256, activation=tf.keras.layers.ReLU(), 
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(v)
     # Transformer layer https://arxiv.org/abs/2010.11929 (LayerNorm layers removed for better performance)
     ma  = MultiHeadAttention(head_size=num_heads, num_heads=num_heads)([q, k, v]) 
-    ma = BatchNormalization()(ma, training=training)
-    ma = Activation('relu')(ma)
+    # ma = BatchNormalization()(ma, training=training)
+    # ma = Activation('relu')(ma)
     return ma
 
 def mix_model(opt, cnn_1d_model, resnet_50, lstm_extracted_model, lstm_condition_model, input_1D, input_2D, input_extracted, input_type, training=False):
@@ -42,8 +42,8 @@ def mix_model(opt, cnn_1d_model, resnet_50, lstm_extracted_model, lstm_condition
   
   merged_value = concatenate([hidden_out_extracted, hidden_out_type], axis=-1, name='merged_value')
   
-  merged_value_0 = TransformerLayer(hidden_out_1D, hidden_out_2D, merged_value, 8, training)
-  merged_value_1 = concatenate([hidden_out_2D, merged_value_0], axis=-1, name='merged_value_1')
+  merged_value_0 = TransformerLayer(hidden_out_1D, hidden_out_2D, merged_value, 4, training)
+  merged_value_1 = concatenate([hidden_out_2D, merged_value_0, hidden_out_1D], axis=-1, name='merged_value_1')
   output = Dense(1, activation='sigmoid')(merged_value_1)
   return output
   
