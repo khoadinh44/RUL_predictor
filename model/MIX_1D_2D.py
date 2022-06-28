048089dc3cb2538329553976a0f5e8206ff27b2f
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-06-28 23:13:02 +0700

    Update MIX_1D_2D.py

--- model/MIX_1D_2D.py
+++ model/MIX_1D_2D.py
@@ -6,6 +6,7 @@ from keras import layers, regularizers
 import keras.backend as K
 
 def TransformerLayer(q, k, v, num_heads=4, training=None):
+    x = v
     q = tf.keras.layers.Dense(256, activation=tf.keras.layers.ReLU(), 
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
@@ -19,30 +20,25 @@ def TransformerLayer(q, k, v, num_heads=4, training=None):
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(v)
     # Transformer layer https://arxiv.org/abs/2010.11929 (LayerNorm layers removed for better performance)
-    ma  = MultiHeadAttention(head_size=num_heads, num_heads=num_heads)([q, k, v]) 
+    ma  = MultiHeadAttention(head_size=num_heads, num_heads=num_heads)([q, k, v]) + x
     ma = BatchNormalization()(ma, training=training)
     ma = Activation('relu')(ma)
     return ma
 
-def mix_model(opt, cnn_1d_model, resnet_50, lstm_extracted_model, lstm_condition_model, input_1D, input_2D, input_extracted, input_type, training=False):
+def mix_model(opt, cnn_1d_model, resnet_50, lstm_extracted_model, input_1D, input_2D, input_extracted, input_type, training=False):
   out_1D = cnn_1d_model(opt, training, input_1D)
   out_2D = resnet_50(opt)(input_2D, training=training)
   out_extracted = lstm_extracted_model(opt, training, input_extracted)
-  out_type = lstm_condition_model(opt, training, input_type)
   
   network_1D = Model(input_1D, out_1D, name='network_1D')
   network_2D = Model(input_2D, out_2D, name='network_2D')
   network_extracted = Model(input_extracted, out_extracted, name='network_extracted')
-  network_type = Model(input_type, out_type, name='network_type')
   
   hidden_out_1D = network_1D([input_1D])
   hidden_out_2D = network_2D([input_2D])
   hidden_out_extracted = network_extracted([input_extracted])
-  hidden_out_type = network_type([input_type])
   
-  merged_value = concatenate([hidden_out_extracted, hidden_out_type], axis=-1, name='merged_value')
-  
-  merged_value_0 = TransformerLayer(hidden_out_1D, hidden_out_2D, merged_value, 4, training)
+  merged_value_0 = TransformerLayer(hidden_out_1D, hidden_out_2D, hidden_out_extracted, 4, training)
   merged_value_1 = concatenate([hidden_out_2D, merged_value_0, hidden_out_1D], axis=-1, name='merged_value_1')
   Condition = Dense(1, activation='softmax')(merged_value_1)
   RUL = Dense(1, activation='sigmoid')(merged_value_1)
