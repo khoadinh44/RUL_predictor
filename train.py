829c2c72e7e06342ce58884f650b86bb8a2eec51
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-07-06 08:21:01 +0700

    Update train.py

--- train.py
+++ train.py
@@ -62,7 +62,7 @@ def main(opt, train_data_rul_1D, train_label_rul_1D, test_data_rul_1D, test_labe
     network = lstm_model(opt, training=True)
   if opt.mix_model:
     input_extracted = Input((14, 2), name='Extracted_LSTM_input')
-    input_1D = Input((2559, 2), name='LSTM_CNN1D_input')
+    input_1D = Input((opt.input_shape, 2), name='LSTM_CNN1D_input')
     input_2D = Input((128, 128, 2), name='CNN_input')
     Condition, RUL = mix_model(opt, lstm_model, resnet_101, lstm_extracted_model, input_1D, input_2D, input_extracted, True)
     network = Model(inputs=[input_1D, input_2D, input_extracted], outputs=[Condition, RUL])
