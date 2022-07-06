7699103c9b739b9f4ce76d8464d992f3e03ca1e4
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-07-06 07:40:52 +0700

    Update train.py

--- train.py
+++ train.py
@@ -18,11 +18,11 @@ callbacks = tf.keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', ver
 def parse_opt(known=False):
     parser = argparse.ArgumentParser()
    
-    parser.add_argument('--input_shape', default=2560, type=int, help='1279 for using fft, 2560 for raw data')
+    parser.add_argument('--input_shape', default=2559, type=int, help='1279 for using fft, 2559 for raw data')
     parser.add_argument('--num_classes', default=1, type=str, help='class condition number: 3, class rul condition: 1')
     parser.add_argument('--model', default='cnn_2d', type=str, help='mix, lstm, dnn, cnn_1d, resnet_cnn_2d, cnn_2d, autoencoder')
     parser.add_argument('--save_dir', default=None, type=str)
-    parser.add_argument('--data_type', default=['2d'], type=str, help='shape of data. They can be 1d, 2d, extract')
+    parser.add_argument('--data_type', default=['2d', '1d', 'extract'], type=str, help='shape of data. They can be 1d, 2d, extract')
     parser.add_argument('--condition', default=None, type=str, help='c_1, c_2, c_3, c_all')
     parser.add_argument('--scaler', default=None, type=str)
     parser.add_argument('--main_dir_colab', default=None, type=str)
@@ -100,7 +100,7 @@ def main(opt, train_data_rul_1D, train_label_rul_1D, test_data_rul_1D, test_labe
 
 if __name__ == '__main__':
   opt = parse_opt()
-  start_save_data(opt)
+  # start_save_data(opt)
   from utils.load_rul_data import train_data_rul_1D, train_label_rul_1D, \
                                   test_data_rul_1D, test_label_rul_1D, \
                                   train_data_rul_2D, \
