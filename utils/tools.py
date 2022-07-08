e0c9198bc245d0d5bf050484e22926305c763fcd
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-07-08 13:42:30 +0700

    Update tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -15,8 +15,42 @@ from sklearn.preprocessing import PowerTransformer
 from utils.extract_features import extracted_feature_of_signal
 from sklearn.metrics import r2_score, accuracy_score
 from os import path
+import scipy
 
+def hankel_svdvals(data, hankel_window_size, slice_window_size):
+    """ Slices data in 'slice_window_size' and compute hankel matrix singular values with 'hankel_window_size' """
 
+    n_slices = len(data)//slice_window_size
+    hankel_svd = []
+
+    for i in range(n_slices):
+        sample_data = data[slice_window_size*i : slice_window_size*(i+1)]
+        c = sample_data[0: hankel_window_size]
+        r = sample_data[hankel_window_size - 1: ]
+        h = scipy.linalg.hankel(c, r)
+
+        hankel_svd.append(scipy.linalg.svdvals(h))
+
+    return hankel_svd
+
+def correlation_coeffs(data, baseline, norm_interval, filter_window_size, filter_polyorder):
+    """ Normalizes data, select baseline data and compute the correlation coefficients """
+
+    a, b = norm_interval
+    diff = b - a
+
+    MIN = min([min(x) for x in data])
+    MAX = max([max(x) for x in data])
+    DIFF = MAX - MIN
+
+    data_norm = [diff*(x - MIN)/(DIFF) + a for x in data]
+    
+    x = data_norm[baseline]
+    
+    R = [sum(np.multiply(x, y))/(np.sqrt(sum(x**2)*sum(y**2))) for y in data_norm]
+
+    # Passing savgol filter
+    return scipy.signal.savgol_filter(R, filter_window_size, filter_polyorder)
 
 #----------------------#### General ####------------------------------------------------
 def recall_m(y_true, y_pred):
@@ -174,13 +208,13 @@ def denoise(signals):
         # all_signal.append(nr.reduce_noise(y=x, sr=2559, hop_length=20, time_constant_s=0.1, prop_decrease=0.5, freq_mask_smooth_hz=25600))
     return np.expand_dims(all_signal, axis=-1)
 
-def convert_to_image(name_bearing, opt, type_data, num_files):
+def convert_to_image(name_bearing, opt, type_data, num_files, time=None):
     data = {'x': [], 'y': []}
     if type_data == '2d':
       print('-'*10, f'Convert to 2D data', '-'*10, '\n')
     else:
       print('-'*10, f'Maintain 1D data', '-'*10, '\n')
-
+    
     for i in range(num_files):
         name = f"/acc_{str(i+1).zfill(5)}.csv"
         file_ = os.path.join(opt.main_dir_colab, name_bearing)+name
@@ -193,6 +227,24 @@ def convert_to_image(name_bearing, opt, type_data, num_files):
             y_ = gen_rms(coef_h)
             data['x'].append(x_)
             data['y'].append(y_)
+            
+    if time != None: # Create label for test set -----------------------------------------
+        data['y'] = np.ones_like(data['y'])
+        data['y'][time: ] = np.linspace(1, 0, len(data['y'][time: ]))
+    else:  # Create label for training set -----------------------------------------
+        window = 100
+        label_new = hankel_svdvals(data['y'], 9, window)
+        label_new = correlation_coeffs(label_new, 0, [-1, 1], 3, 2)
+        
+        kmeans = KMeans(n_clusters=2, random_state=0).fit(label_new.reshape(-1, 1))
+        thres = np.array(kmeans.labels_).astype(np.int32) > 0.5
+        PSDclean = label_new * thres
+        
+        unique, counts = np.unique(PSDclean, return_counts=True)
+        normal_time = counts[0]*(window//2)
+        label_train = np.ones_like(data['y'])
+        label_train[normal_time:] = np.linspace(1, 0, len(label_train[normal_time:]))
+        data['y'] = label_train
         
     if type_data=='extract':
       print('-'*10, 'Convert to Extracted data', '-'*10, '\n')
@@ -234,9 +286,8 @@ def convert_to_image(name_bearing, opt, type_data, num_files):
     else:
       print('-'*10, 'Raw data', '-'*10, '\n')
       data['x'] = np.array(data['x'])
-    
-    # data['y'], _ = fit_values(2.31e-5, 0.99, 1.10, 1.68e-93, 28.58, np.array(data['y']))
-    data['y'] = convert_1_to_0(data['y'])
+#     data['y'], _ = fit_values(2.31e-5, 0.99, 1.10, 1.68e-93, 28.58, np.array(data['y']))
+#     data['y'] = convert_1_to_0(data['y'])
 
     x_shape = data['x'].shape
     y_shape = data['y'].shape
