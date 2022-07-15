55e729d18f77d0beab63072353095fc1526ad745
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-07-15 12:53:30 +0700

    Update tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -344,7 +344,13 @@ def convert_1_to_0(data):
       f_data = np.ones_like(data)
     return 1-f_data
 
-def predict_time(h):
+def predict_time(data):
+  h = []
+  for i in np.squeeze(data[:, :, 0]):
+    # g = kurtosis(i)
+    g = gen_rms(i)
+    h.append(g)
+    
   h0 = convert_1_to_0(h)
   length_seg = 80
   num_seg = len(h0)//length_seg
